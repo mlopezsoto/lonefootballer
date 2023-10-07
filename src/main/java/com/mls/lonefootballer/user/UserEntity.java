a0555962fe706fba272f013363d6262c89c647c3
Mauricio Lopez-Soto <mlopezsoto@gmail.com>
2023-10-08 10:17:50 +1100

    Small refactoring, class renaming.

--- src/main/java/com/mls/lonefootballer/user/UserEntity.java
+++ src/main/java/com/mls/lonefootballer/user/UserEntity.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/mls/lonefootballer/user/UserRepository.java
+++ src/main/java/com/mls/lonefootballer/user/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     List<UserRecord> findBy();
 }
